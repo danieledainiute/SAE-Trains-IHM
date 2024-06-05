55a212e68ba9b7c1f00e6a8a232a2d3b45846bfb
danieledainiute <147133551+danieledainiute@users.noreply.github.com>
2024-06-05 14:46:57 +0200

    premier partie de demarrage

--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueDuJeu.java
@@ -2,12 +2,14 @@ package fr.umontpellier.iut.trainsJavaFX.vues;
 
 import fr.umontpellier.iut.trainsJavaFX.IJeu;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
- *
+ * <p>
  * Elle est initialisée avec une référence sur la partie en cours (Jeu).
  * <p>
  * On y définit les bindings sur les éléments internes qui peuvent changer
@@ -18,16 +20,30 @@ public class VueDuJeu extends Pane {
 
     private final IJeu jeu;
     private VuePlateau plateau;
+    private Label instruction;
+    private Label nomJoueur;
+    private Button passer;
+
 
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
         plateau = new VuePlateau();
-        getChildren().add(plateau);
+        instruction = new Label();
+        nomJoueur = new Label();
+        passer = new Button("Passer");
+        passer.setOnMouseClicked(event -> jeu.passerAEteChoisi());
+
+        getChildren().addAll(plateau, instruction, nomJoueur, passer);
     }
 
     public void creerBindings() {
         plateau.prefWidthProperty().bind(getScene().widthProperty());
         plateau.prefHeightProperty().bind(getScene().heightProperty());
+        instruction.textProperty().bind(jeu.instructionProperty());
+
+        jeu.joueurCourantProperty().addListener((observable, oldValue, newValue) -> {
+            nomJoueur.setText("Joueur : " + newValue.getNom());
+        });
         plateau.creerBindings();
     }
 
